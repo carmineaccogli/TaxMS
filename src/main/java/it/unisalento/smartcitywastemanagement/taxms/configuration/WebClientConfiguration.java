956ee2a23935e60d12286f56ef45b851639c5691
carmineaccogli <112572172+carmineaccogli@users.noreply.github.com>
2023-09-19 20:05:48 +0200

    Change the pd port in WebClientConfiguration

--- src/main/java/it/unisalento/smartcitywastemanagement/taxms/configuration/WebClientConfiguration.java
+++ src/main/java/it/unisalento/smartcitywastemanagement/taxms/configuration/WebClientConfiguration.java
@@ -9,7 +9,7 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class WebClientConfiguration {
 
-    private final String API_DISPOSAL_MS = "http://disposalManagementService:8083/api/disposal";
+    private final String API_DISPOSAL_MS = "http://disposalManagementService:8080/api/disposal";
 
 
     @Bean
