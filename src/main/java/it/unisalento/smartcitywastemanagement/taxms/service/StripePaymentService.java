dfb43cdc5fa94ec92d486f9863d96640b34b095b
carmineaccogli <112572172+carmineaccogli@users.noreply.github.com>
2023-09-24 11:54:22 +0200

    Implemented changeCustomerCard method

--- src/main/java/it/unisalento/smartcitywastemanagement/taxms/service/StripePaymentService.java
+++ src/main/java/it/unisalento/smartcitywastemanagement/taxms/service/StripePaymentService.java
@@ -8,7 +8,12 @@ public interface StripePaymentService {
 
     Charge chargeNewCard(String token, double amount) throws Exception;
 
-    Charge chargeCustomerCard(String customerId, double amount) throws Exception;
 
     Customer createCustomer(String token, String email, String fullName) throws Exception;
+
+    Charge chargeCustomerCard(String customerId, double amount, String token) throws Exception;
+
+    Customer getCustomerByEmail(String email) throws Exception;
+
+    Charge chargeCustomerDefaultCard(String customerId, double amount) throws Exception;
 }
--- src/main/java/it/unisalento/smartcitywastemanagement/taxms/service/StripePaymentServiceImpl.java
+++ src/main/java/it/unisalento/smartcitywastemanagement/taxms/service/StripePaymentServiceImpl.java
@@ -1,8 +1,9 @@
 package it.unisalento.smartcitywastemanagement.taxms.service;
 
 import com.stripe.Stripe;
-import com.stripe.model.Charge;
-import com.stripe.model.Customer;
+import com.stripe.model.*;
+import com.stripe.param.CustomerListParams;
+import com.stripe.param.CustomerRetrieveParams;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -52,5 +53,98 @@ public class StripePaymentServiceImpl implements StripePaymentService{
         Charge charge = Charge.create(chargeParams);
         return charge;
     }
+
+    /** FUNZIONE PER EFFETTUARE IL PAGAMENTO TRAMITE STRIPE
+     * 1. Creo l'oggetto di pagamento impostando i campi base
+     * 2. Espando le sorgenti di pagamento per il cliente e ottengo le informazioni sul cliente
+     * 3. Ottengo altre informazioni aggiuntive dal token che saranno necessarie
+     * 4. Itero sulle sorgenti di pagamento
+     *      4.1 Se il metodo di pagamento è una card con stessa impronta di quella definita dal token, allora salvo l'id della sorgente
+     * 5. Check di questo id
+     *      5.1 Se non è nullo, uso tale metodo di pagamento e lo indico nell'oggetto charge
+     *      5.2 Altrimenti creo il nuovo metodo di pagamento e lo uso per concludere la transazione
+     * 6. Effettuo il pagamento
+     **/
+    public Charge chargeCustomerCard(String customerId, double amount, String token) throws Exception {
+
+
+        // 1
+        Map<String, Object> chargeParams = new HashMap<String, Object>();
+        chargeParams.put("amount", (int) (amount * 100));
+        chargeParams.put("currency", "EUR");
+        chargeParams.put("customer", customerId);
+
+        // 2
+        CustomerRetrieveParams params = CustomerRetrieveParams.builder()
+                .addExpand("sources").build();
+        Customer customer = Customer.retrieve(customerId, params, null);
+
+        // 3
+        Card customerCard= Token.retrieve(token).getCard();
+        String id = Token.retrieve(token).getId();
+
+        String sourceID =null;
+
+        // 4
+        for (PaymentSource source : customer.getSources().getData()) {
+            // 4.1
+            if (source instanceof Card && ((Card) source).getFingerprint().equals(customerCard.getFingerprint())) {
+                sourceID = source.getId();
+                break;
+            }
+        }
+
+        // 5
+        if(sourceID != null) {
+            // 5.1
+            chargeParams.put("source",sourceID);
+        }
+        else {
+            // 5.2
+            Map<String, Object> sourceParams = new HashMap<String,Object>();
+            sourceParams.put("source",id);
+
+            PaymentSource newSource = customer.getSources().create(sourceParams);
+
+            chargeParams.put("source", newSource.getId());
+        }
+
+
+        // 6
+        Charge charge = Charge.create(chargeParams);
+
+        return charge;
+    }
+
+    public Customer getCustomerByEmail(String email) throws Exception {
+
+        // Crea un oggetto CustomerListParams per filtrare i clienti per indirizzo email
+        CustomerListParams listParams = CustomerListParams.builder()
+                .setEmail(email)
+                .build();
+
+        // Esegui la query per ottenere i clienti con lo stesso indirizzo email
+        Iterable<Customer> customers = Customer.list(listParams).autoPagingIterable();
+
+        // Restituisci il primo cliente corrispondente (se presente)
+        for (Customer customer : customers) {
+            return customer;
+        }
+
+        // Se non viene trovato alcun cliente corrispondente, restituisci null
+        return null;
+    }
+
+    public Charge chargeCustomerDefaultCard(String customerId, double amount) throws Exception {
+        String sourceCard = getCustomer(customerId).getDefaultSource();
+        Map<String, Object> chargeParams = new HashMap<String, Object>();
+        chargeParams.put("amount", (int) (amount * 100));
+        chargeParams.put("currency", "EUR");
+        chargeParams.put("customer", customerId);
+        chargeParams.put("source", sourceCard);
+        Charge charge = Charge.create(chargeParams);
+        return charge;
+    }
+
 }
 
