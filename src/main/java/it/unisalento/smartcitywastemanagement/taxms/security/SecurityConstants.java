b9842e31ce6f7d7c83458f8838a7d4b329dd6097
carmineaccogli <112572172+carmineaccogli@users.noreply.github.com>
2023-10-06 23:10:43 +0200

    Fixed authenticated communication between TaxMS and disposalMS

--- src/main/java/it/unisalento/smartcitywastemanagement/taxms/security/SecurityConstants.java
+++ src/main/java/it/unisalento/smartcitywastemanagement/taxms/security/SecurityConstants.java
@@ -1,7 +1,19 @@
 package it.unisalento.smartcitywastemanagement.taxms.security;
 
+import java.util.List;
+
 public class SecurityConstants {
     public static final String JWT_SECRET = "seedseedseedseedseedseedseedseedseedseedseed"; // sarà utilizzato per l'algoritmo di firma
 
     public static final String THIS_MICROSERVICE ="http://taxService:8085";
+
+    public static final String ISSUER ="http://taxService:8085";
+
+
+    public static final List<String> AUDIENCE = List.of("http://disposalManagementService:8083");
+
+    public static final String SUBJECT = "taxMS";
+
+    public static final String ROLE = "MICROSERVICE-COMMUNICATION";
+
 }
--- src/main/java/it/unisalento/smartcitywastemanagement/taxms/service/ApiServiceImpl.java
+++ src/main/java/it/unisalento/smartcitywastemanagement/taxms/service/ApiServiceImpl.java
@@ -2,7 +2,9 @@ package it.unisalento.smartcitywastemanagement.taxms.service;
 
 
 import it.unisalento.smartcitywastemanagement.taxms.dto.CitizenWasteMetricsDTO;
+import it.unisalento.smartcitywastemanagement.taxms.security.JwtUtilities;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -18,10 +20,16 @@ public class ApiServiceImpl implements ApiService {
     @Autowired
     WebClient disposalDataWebClient;
 
+    @Autowired
+    private JwtUtilities jwtUtilities;
+
     public Mono<List<CitizenWasteMetricsDTO>> APICALL_getDisposalData(int year) {
+
+        final String jwtToken = jwtUtilities.generateToken();
 
         return disposalDataWebClient.get()
                 .uri("/metrics/year/{year}", year)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken)
                 .retrieve()
                 .bodyToFlux(CitizenWasteMetricsDTO.class)
                 .collectList();
