96f99e09ddde853f7b2e39c6fdf786f18d907f4a
carmineaccogli <112572172+carmineaccogli@users.noreply.github.com>
2023-10-06 11:14:31 +0200

    Changed THIS_MICROSERVICE

--- src/main/java/it/unisalento/smartcitywastemanagement/taxms/security/SecurityConstants.java
+++ src/main/java/it/unisalento/smartcitywastemanagement/taxms/security/SecurityConstants.java
@@ -3,5 +3,5 @@ package it.unisalento.smartcitywastemanagement.taxms.security;
 public class SecurityConstants {
     public static final String JWT_SECRET = "seedseedseedseedseedseedseedseedseedseedseed"; // sarà utilizzato per l'algoritmo di firma
 
-    public static final String THIS_MICROSERVICE ="http://taxService:8084";
+    public static final String THIS_MICROSERVICE ="http://taxService:8085";
 }
