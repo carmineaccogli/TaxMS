eba47a8566b2fa0c53bd2b001d341a058a8d62eb
carmineaccogli <112572172+carmineaccogli@users.noreply.github.com>
2023-10-06 10:47:51 +0200

    Implemented Security with JWT Token

---
+++ src/main/java/it/unisalento/smartcitywastemanagement/taxms/security/SecurityConstants.java
@@ -0,0 +1,7 @@
+package it.unisalento.smartcitywastemanagement.taxms.security;
+
+public class SecurityConstants {
+    public static final String JWT_SECRET = "seedseedseedseedseedseedseedseedseedseedseed"; // sarà utilizzato per l'algoritmo di firma
+
+    public static final String THIS_MICROSERVICE ="http://taxService:8084";
+}
